e677ff2f7d7e1dc6b4a7730d5ba5c657fdbb6c72
Andrei <andrey.laptev@outlook.com>
2020-02-15 17:12:07 -0500

    moved to repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import * as questions  from '../assets/questions.json';
+
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular 6';
+export class AppComponent {
+  title = 'Quiz';
+
+ start: boolean = false;
+ currentQuestion = questions[0]
+ questionsCounter: number = 0;
+ numberOfQuestons: number;
+ score: number = 0;
+ end: boolean = false
+ message: string;
+
+  ngOnInit(){
+    this.numberOfQuestons = questions['length']
+  }
+
+  onStart(){
+    this.start = !this.start;
+    this.questionsCounter += this.questionsCounter;
+  }
+
+  changeQuestion(e){
+    if (this.currentQuestion.correctAnswer == e) {
+      this.score = this.score + 1;
+      sessionStorage.setItem('score', this.score.toString());
+    } 
+    this.questionsCounter ++;
+    this.questionsCounter == this.numberOfQuestons ? this.endQuiz() : this.currentQuestion = questions[this.questionsCounter]  
+  }
+
+      endQuiz(){
+        this.end = true;
+        if(sessionStorage.getItem('score') == null) sessionStorage.setItem('score', '0')
+        this.message = "Quiz is over. Your total score is " + sessionStorage.getItem('score') + " out of  " + this.numberOfQuestons;
+        sessionStorage.clear();
+    }
+
+    retake() {
+      window.location.reload();
+    }
 }
